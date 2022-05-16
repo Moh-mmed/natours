94e0f99b8e4290fc81974111b8bb82cfe28ec15a
mohammedBnomr <moohaboy@gmail.com>
2022-05-16 18:29:18 +0300

    Update CORS

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -25,6 +26,11 @@ app.enable('trust proxy');
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
+
+//* Implement CORS
+app.use(cors());
+
+app.options('*', cors());
 
 //* Set security HTTP headers
 app.use(helmet());
