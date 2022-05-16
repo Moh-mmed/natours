d7156f992e4a4dbe03c94f6dee1ec7fdfac3abb8
mohammedBnomr <moohaboy@gmail.com>
2022-05-16 22:02:48 +0300

    v1.0.6

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -41,7 +41,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.display_items[0].amount / 1000;
+  const price = session.amount_total / 1000;
   await Booking.create({ tour, user, price });
 };
 exports.webhookCheckout = (req, res, next) => {
