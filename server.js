d705876f68299040c33b896c874281a44127c073
mohammedBnomr <moohaboy@gmail.com>
2022-05-16 18:52:40 +0300

    Fix process exit

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ process.on('unhandledRejection', (err) => {
   });
 });
 
-process.exit('SIGTERM', () => {
+process.on('SIGTERM', () => {
   console.log('SIGTERM RECEIVED. Shutting down gracefully');
   server.close(() => {
     console.log('Process terminated!');
